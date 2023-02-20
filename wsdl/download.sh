9fc95bb6d26be0e6d59b37d6332d9e287df2df8c
Harald Sitter <sitter@kde.org>
2023-02-20 13:23:26 +0100

    drop excess license headers
    spdx is good enough

--- wsdl/download.sh
+++ wsdl/download.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 set -eu
 
-# Copyright (C) 2019 Casper Meijn <casper@meijn.net>
+# SPDX-FileCopyrightText: 2019 Casper Meijn <casper@meijn.net>
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # First download WS-Discovery 2005-04
 wget --no-verbose -r http://schemas.xmlsoap.org/ws/2005/04/discovery/ws-discovery.xsd
